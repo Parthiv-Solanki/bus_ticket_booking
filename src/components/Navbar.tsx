a04ff956919e984155cfb5194fbe43c15cbbabf5
GHOST-BOOTING <solankiparth304@gmail.com>
2024-06-06 11:37:09 +0530

    handle the active route styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
     return (
@@ -7,8 +7,8 @@ const Navbar: React.FC = () => {
             <div className="container mx-auto flex justify-between">
                 <div className="text-white text-xl">Bus Ticket Booking</div>
                 <div className="text-white">
-                    <Link to="/reservation" className="mx-2">Reservation</Link>
-                    <Link to="/dashboard" className="mx-2">Dashboard</Link>
+                    <NavLink to="/reservation" className="mx-2">Reservation</NavLink>
+                    <NavLink to="/dashboard" className="mx-2">Dashboard</NavLink>
                 </div>
             </div>
         </nav>
