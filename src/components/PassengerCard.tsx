3581482234cc08cf7a2f613e1c733efba3c1ae15
GHOST <solankiparth304@gmail.com>
2024-12-01 10:35:35 +0530

    remove unused import

--- src/components/PassengerCard.tsx
+++ src/components/PassengerCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { PassengerCardProps } from '../utils';
 
 const PassengerCard: React.FC<PassengerCardProps> = ({ passenger, reservedSeats, onEdit, onDelete }) => {
