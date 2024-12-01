609dcd06dc1d607c41580543519ac7c4b5c43519
GHOST <solankiparth304@gmail.com>
2024-12-01 10:32:25 +0530

    changes: favicon, routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Navigate, createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Reservation from './pages/Reservation';
 import Dashboard from './pages/Dashboard';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/reservation" replace />,
+  },
+  {
+    path: "/reservation",
+    element: (
+      <>
+        <Navbar />
+        <Reservation />
+      </>
+    ),
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <>
+        <Navbar />
+        <Dashboard />
+      </>
+    ),
+  },
+]);
+
+
 const App: React.FC = () => {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Reservation />} />
-        <Route path="/reservation" element={<Reservation />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <RouterProvider router={router} />
   );
 }
 
