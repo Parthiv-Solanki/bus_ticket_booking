fe78a72f1276fa5e39c0a41a10850cfe73395459
GHOST <solankiparth304@gmail.com>
2024-12-02 21:49:36 +0530

    style changes

--- src/components/PassengerList.tsx
+++ src/components/PassengerList.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import PassengerCard from './PassengerCard';
 import { PassengerListProps } from '../utils';
+import { Link } from 'react-router-dom';
 
 const PassengerList: React.FC<PassengerListProps> = ({ passengers, reservedSeats, onEdit, onDelete }) => {
     return (
         <div className='grid lg:grid-cols-2 xl:grid-cols-3 gap-4'>
-            {passengers.map((passenger) => (
+            {passengers && passengers.length > 0 ? passengers?.map((passenger) => (
                 <PassengerCard
                     key={passenger.seatNumber}
                     passenger={passenger}
@@ -13,7 +14,12 @@ const PassengerList: React.FC<PassengerListProps> = ({ passengers, reservedSeats
                     onEdit={onEdit}
                     onDelete={onDelete}
                 />
-            ))}
+            ))
+                :
+                <Link to="/reservation" className='text-[#ffa500] hover:border p-2 bg-gray-800 rounded hover:border-[#ffa500] w-fit'>
+                    Click Here to Book Your First Ticket
+                </Link>
+            }
         </div>
     );
 };
