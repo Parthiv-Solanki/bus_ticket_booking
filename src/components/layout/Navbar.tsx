f00530ff398452bd20f427dd124d2c08b788c74d
GHOST <solankiparth304@gmail.com>
2024-12-14 21:33:30 +0530

    changed logo

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className="bg-gray-800 p-4">
-            <div className="container mx-auto flex justify-between">
-                <a href='/reservation' className="text-white md:text-xl">Bus Ticket Booking</a>
+            <div className="container mx-auto flex justify-between items-center">
+                <a href='/reservation' className="text-white md:text-sm flex gap-2 justify-enter items-center">
+                    <div className='w-8 h-8'>
+                        <img src='./bus.png' alt='bus-logo' className='w-full h-full' />
+                    </div>
+                    <span className='text-nowrap hover:text-[#ffa500]'>Bus Trip Planner</span>
+                </a>
                 <div className="text-white">
                     <NavLink to="/reservation" className="mx-2 hover:text-[#ffa500]">Reservation</NavLink>
                     <NavLink to="/dashboard" className="mx-2 hover:text-[#ffa500]">Dashboard</NavLink>
