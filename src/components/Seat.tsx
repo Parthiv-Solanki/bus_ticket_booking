f7319e94b4e7635dd0f7e1b2c3e5d99e5ac5cb86
GHOST <solankiparth304@gmail.com>
2024-12-19 21:27:46 +0530

    style changes

--- src/components/Seat.tsx
+++ src/components/Seat.tsx
@@ -6,11 +6,11 @@ const Seat: React.FC<SeatProps> = (props) => {
     return (
         <button
             onClick={onClick}
-            className={`text-end w-20 h-10 m-1 border-2 border-black cursor-pointer rounded ${isReserved ? 'bg-red-300 hover:bg-red-600' : 'bg-gray-300 hover:bg-gray-500 '}`}
+            className={`group text-end w-20 h-10 m-1 border-2 border-black cursor-pointer rounded hover:text-white ${isReserved ? 'bg-red-300 hover:bg-red-600' : 'bg-gray-300 hover:bg-gray-500'}`}
             disabled={isReserved}
         >
             <span>{seatNumber}</span>
-            <span className='h-2 w-5 border-2 border-black mx-2 px-1'></span>
+            <span className='h-2 w-5 border-2 border-black mx-2 px-1 group-hover:border-white'></span>
         </button>
     );
 }
